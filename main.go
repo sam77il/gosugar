b6f101751633043313d76cd1e2e295935b349788
sam77il <yavuzsamil.guengoer@gmail.com>
2026-05-13 09:06:02 +0200

    changed directory

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"gosugar/sugar"
+)
+
+func main() {
+	server := sugar.New(sugar.Config{
+		Port: 8080,
+	})
+	// creating router
+	router := server.Router()
+
+	router.Static("./static", "/files")
+
+	server.Middleware("/ipa/*", func(ctx *sugar.SugarContext) error {
+		fmt.Println(ctx.Request.Method)
+		return ctx.Request.Next()
+	})
+
+	ipa := server.Group("/ipa")
+	ipa.Get("/users", func (ctx *sugar.SugarContext) error {
+		return ctx.Response.Status(200).JSON(sugar.J{"success": true})
+	})
+
+	router.Get("/api", func (ctx *sugar.SugarContext) error {
+		if ctx.Request.Header.Get("lol") == "yes" {
+			ctx.Request.Next()
+			return nil
+		}
+
+		return ctx.Response.Status(400).JSON(map[string]any{"success": false, "message": "wrong method"})
+	}, func (ctx *sugar.SugarContext) error  {
+		fmt.Println("yes lol")
+
+		if ctx.Request.Header.Get("goofy") == "yes" {
+			ctx.Request.Next()
+			return nil
+		}
+		return ctx.Response.Status(400).JSON(sugar.J{"success": false})
+	}, func (ctx *sugar.SugarContext) error {
+		return ctx.Response.Status(200).JSON(map[string]any{"success": true})
+	})
+
+	server.Listen()
+}
